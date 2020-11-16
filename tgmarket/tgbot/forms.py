2ec7a4652428959c2fcf1dd1af48b968532c8567
Noway2w1n <dimamoving@gmail.com>
2020-11-16 21:31:04 +0300

    Django models for django admin

---
+++ tgmarket/tgbot/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import Profile
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = (
+            "phone_number",
+            "name",
+            "purchase",
+            "product",
+            "extra_message",
+
+        )
+        widgets = {
+            "name": forms.TextInput,
+        }
--- tgmarket/tgbot/models.py
+++ tgmarket/tgbot/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    phone_number = models.CharField(verbose_name="User number from telegram", max_length=13, default="00000000000000")
+    name = models.TextField(verbose_name="User name")
+    purchase = models.BooleanField(default=False)
+    product = models.TextField(verbose_name="products to purchase", blank=True)
+    extra_message = models.TextField(verbose_name="Some extra text from buyer", blank=True)
+
+
+
+class Meta:
+    verbose_name = "Profile"
+
+class Product(models.Model):
+    product_type = models.TextField(verbose_name="type of product")
+    product_name = models.TextField(verbose_name="name of product")
+    product_price = models.PositiveIntegerField(verbose_name="price of product")
+    product_description = models.TextField(verbose_name="description of product", blank=True)
