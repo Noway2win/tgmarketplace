e4d5fef3f04e2ddf08845d0ea13f7923bcad2c45
Noway2w1n <dimamoving@gmail.com>
2020-11-18 23:39:11 +0300

    Configured Dynaconf for django, find out that it has a conflict with dj_database_url

--- src/tgmarket/settings.py
+++ src/tgmarket/settings.py
@@ -9,10 +9,14 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
+
+from dynaconf.default_settings import ENV_FOR_DYNACONF
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "u7lm@zq0bwme&41ofc&bz%^x_^_y!oiwil=^=a)=dal+-s^!(p"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "telegrammarket.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,14 +78,11 @@ WSGI_APPLICATION = "tgmarket.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
+DATABASE_URL = ""
+db_url = DATABASE_URL
+if ENV_FOR_DYNACONF == "heroku":
+    db_url = os.getenv("DATABASE_URL")
+DATABASES = {"default": {}}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# Read more at https://dynaconf.readthedocs.io/en/latest/guides/django.html
+import dynaconf  # noqa
+
+settings = dynaconf.DjangoDynaconf(__name__)  # noqa
+# HERE ENDS DYNACONF EXTENSION LOAD (No more code below this line)
