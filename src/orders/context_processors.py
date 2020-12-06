eb5c77296da2e6b63489bd6a32db14a6fd218827
Noway2w1n <dimamoving@gmail.com>
2020-12-06 21:13:41 +0300

    trying to deploy on heroku

--- src/orders/context_processors.py
+++ src/orders/context_processors.py
@@ -4,7 +4,7 @@ from .models import ProductBasket
 def basket_info_func(request):
     session_key = request.session.session_key
     if not session_key:
-        request.session.session_key=1
+        request.session["session_key"] = 123
         request.session.cycle_key()
     basket_products = ProductBasket.objects.filter(
         session_key=session_key, is_active=True
