16339a5d68730367735095e5e56dada1f4d09c99
Noway2w1n <dimamoving@gmail.com>
2020-12-06 21:28:10 +0300

    trying to deploy on heroku

--- src/orders/context_processors.py
+++ src/orders/context_processors.py
@@ -6,11 +6,15 @@ def basket_info_func(request):
     if not session_key:
         request.session["session_key"] = 123
         request.session.cycle_key()
-    basket_products = ProductBasket.objects.filter(
-        session_key=session_key, is_active=True
-    )
-    if basket_products:
-        total_nmb = basket_products.count()
-    else:
+    try:
+        basket_products = ProductBasket.objects.filter(
+            session_key=session_key, is_active=True
+        )
+        if basket_products:
+            total_nmb = basket_products.count()
+        else:
+            total_nmb = 0
+    except:
         total_nmb = 0
+
     return locals()
